b4a77ea142bcee63070589ecafe2738b94890ad3
Archilir <32329155+Archilir@users.noreply.github.com>
2020-10-27 22:08:10 +0100

    38. Count and Say (easy, java)

---
+++ easy/038-count-and-say/java/Runner.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Runner {
+    public static void main(String args[]) {
+        int n = 30;
+        Solution solver = new Solution();
+
+        System.out.println("Input: n = " + n);
+        String solution = solver.countAndSay(n);
+        System.out.println("Output: \"" + solution + "\"");
+    }
+}
